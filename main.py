4222596d15a719f30b4e4201546ebd63a4b8b458
Cherkasik <ilyakopeysk@gmail.com>
2018-12-16 19:43:01 +0300

    working algos with creating test and train sets

--- main.py
+++ main.py
@@ -6,14 +6,21 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
 dataset = pd.read_csv("train.csv")  # loading dataset
-dataset.fillna(-99999, inplace=True)
+dataset = dataset.apply(pd.to_numeric, errors='coerce')
+dataset.fillna(0, inplace=True)
 
 forecast_out = int(math.ceil(0.01*len(dataset)))
 
 X = np.array(dataset.values[:, 2:377]) #  data features
 Y = np.array(dataset.values[:, 1]) #  data label (what we predict)
 
-#  X = preprocessing.scale(X)
+X = preprocessing.scale(X)
 dataset.dropna(inplace=True)
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+
+clf = LinearRegression()
+clf.fit(X_train, Y_train)
+accuracy = clf.score(X_test, Y_test)
+
+print (accuracy)
